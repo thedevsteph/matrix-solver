73f824a0021f6788dc605e5a6b0c63c52465c870
Stephanie <stephanie.o.fashagba@gmail.com>
2018-11-03 22:14:15 +0000

    written tests to subtract matrices

--- matrices/src/matrices/tests/subtractTwoMatricesTest.java
+++ matrices/src/matrices/tests/subtractTwoMatricesTest.java
@@ -1,14 +1,17 @@
-package matrices;
+package matrices.tests;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import matrices.Matrix;
 
-class addTwoMatrices {
+class subtractTwoMatricesTest {
 
 	 private Matrix matrix; 
-	
+		
 	 @BeforeEach
 	 public void newMatrix() {
 		 matrix = new Matrix();
@@ -19,28 +22,28 @@ class addTwoMatrices {
 	public void throwsErrorIfMatricesHaveDifferentDimensions() throws Exception{
 		int[][] firstMatrix = {{1, 3, 6}, {3,4, 6}, {7,2,11}};
 		int[][] secondMatrix = {{1,2, 2}, {3,2, 4}};
-		matrix.addMatrices(firstMatrix, secondMatrix);	
+		matrix.subtractMatrices(firstMatrix, secondMatrix);	
 	}
 		
 	@Test
-	public void addingMatricesAThreeByTwo(){
+	public void subtractingMatricesThreeByTwo(){
 		int[][] firstMatrix = {{1, 3, 6}, {3, 4, 6}};
 		int[][] secondMatrix = {{1,2, 2}, {3, 2, 4}};
 		int[][] result =  {{2, 5, 8}, {6, 6, 10}};
-		matrix.addMatrices(firstMatrix, secondMatrix);
-		assertArrayEquals(matrix.addMatrices(firstMatrix, secondMatrix), result);	
+		matrix.subtractMatrices(firstMatrix, secondMatrix);
+		assertArrayEquals(matrix.subtractMatrices(firstMatrix, secondMatrix), result);	
 	}
 	
 	@Test
-	public void addingMatricesAThreeByThree(){
+	public void subtractingMatricesAThreeByThree(){
 		int[][] firstMatrix = {{1, 3, 6}, {3,4, 6}, {7,2,11}};
 		int[][] secondMatrix = {{1,2, 2}, {3,2, 4}, {2,3,4}};
 		int[][] result =  {{2 ,5, 8}, {6,6, 10}, {9,5,15}};
-		matrix.addMatrices(firstMatrix, secondMatrix);
-		assertArrayEquals(matrix.addMatrices(firstMatrix, secondMatrix), result);	
+		matrix.subtractMatrices(firstMatrix, secondMatrix);
+		assertArrayEquals(matrix.subtractMatrices(firstMatrix, secondMatrix), result);	
 	}
 	
 	
 	
 
-};
+}
