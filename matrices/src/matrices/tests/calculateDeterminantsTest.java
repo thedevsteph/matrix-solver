4cfc259d04b35d1471237129934211d4973ebb59
Stephanie <stephanie.o.fashagba@gmail.com>
2018-11-03 22:39:57 +0000

    renamed class name for recursion

--- matrices/src/matrices/tests/calculateDeterminantsTest.java
+++ matrices/src/matrices/tests/calculateDeterminantsTest.java
@@ -13,27 +13,35 @@ class calculateDeterminantsTest {
 		
 	 @BeforeEach
 	 public void newMatrix() {
-		 matrix = new Matrix();
+		 determinant = new determinantSolver();
 	 }
 	 
 	 @Test
 	 public void getDeterminantOfTwoByTwoMatrix() {
 		 int[][] chosenMatrix = {{3,8}, {4, 6}};
-		 assertEquals(matrix.getDeterminant(chosenMatrix), -14);
+		 assertEquals(determinant.solve(chosenMatrix), -14);
 	 }
 	 
 	 @Test
 	 public void getDeterminantOfThreeByThreeMatrix() {
 		 int[][] chosenMatrix = {{6,1,1}, {4,-2,5}, {2,8,7}};
-		 assertEquals(matrix.getDeterminant(chosenMatrix), -306);
+		 assertEquals(determinant.solve(chosenMatrix), -306);
 	 }
+	 
 	 
 	 @Test
 	 public void throwsExceptionIfMatrixIsNotSquare() throws Exception{
 		 int[][] chosenMatrix = {{1,1}, {2,2,2}};
-		 matrix.getDeterminant(chosenMatrix);
+		 determinant.solve(chosenMatrix);
 		 
 	 }
+	 
+	 @Test
+	 public void ifMatrixLengthIsOne() {
+		 int[][] chosenMatrix = {{1}};
+		 assertEquals(determinant.solve(chosenMatrix), 1);
+	 }
+	 
 	 
 	 
 }
