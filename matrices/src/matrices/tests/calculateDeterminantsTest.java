6d65032d8469e7552b80b1ee87bb5b7d9504f780
Stephanie <stephanie.o.fashagba@gmail.com>
2018-11-03 23:27:15 +0000

    simplified tests

--- matrices/src/matrices/tests/calculateDeterminantsTest.java
+++ matrices/src/matrices/tests/calculateDeterminantsTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import matrices.DeterminantSolver;
 import matrices.Matrix;
 
 class calculateDeterminantsTest {
@@ -13,19 +14,19 @@ class calculateDeterminantsTest {
 		
 	 @BeforeEach
 	 public void newMatrix() {
-		 determinant = new determinantSolver();
+		 determinant = new DeterminantSolver();
 	 }
 	 
 	 @Test
 	 public void getDeterminantOfTwoByTwoMatrix() {
 		 int[][] chosenMatrix = {{3,8}, {4, 6}};
-		 assertEquals(determinant.solve(chosenMatrix), -14);
+		 assertEquals(determinant.twoByTwoSolver(chosenMatrix), -14);
 	 }
 	 
 	 @Test
 	 public void getDeterminantOfThreeByThreeMatrix() {
 		 int[][] chosenMatrix = {{6,1,1}, {4,-2,5}, {2,8,7}};
-		 assertEquals(determinant.solve(chosenMatrix), -306);
+		 assertEquals(determinant.threeByThree(chosenMatrix), -306);
 	 }
 	 
 	 
@@ -35,12 +36,7 @@ class calculateDeterminantsTest {
 		 determinant.solve(chosenMatrix);
 		 
 	 }
-	 
-	 @Test
-	 public void ifMatrixLengthIsOne() {
-		 int[][] chosenMatrix = {{1}};
-		 assertEquals(determinant.solve(chosenMatrix), 1);
-	 }
+
 	 
 	 
 	 
